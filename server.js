2d60034f52e6056e459ff2add23e2440b7981e05
webdemo <kienbkav@gmail.com>
2019-01-27 16:25:58 -0800

    testing

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require('express');
 const app = express();
 const request = require('request');
 
-
-app.get('/',function(req,res,next) {
-    var pipe      = req.pipe(request.get('https://api.ipify.org/?format=json'));
-    var response  = [];
-
-    pipe.on('data',function(chunk) {
-        res.send(chunk.toString());
-    });
-
-
+let getData = (req)=>{
+    return new Promise(resolve=>{
+        let pipe = req.pipe(request.get('https://api.ipify.org/?format=json'));
+        let result = '';
+        pipe.on('data',function(chunk) {
+            result += chunk.toString();
+        });
+        pipe.on('end',function(){
+            resolve(result)
+        })
+    })
+};
+app.get('/',async function(req,res,next) {
+    let data = await getData(req);
+    res.send(data)
 })
 app.listen(process.env.PORT || 3000)
