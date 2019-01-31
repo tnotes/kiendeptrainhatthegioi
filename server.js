df477ed0af57bedf159ab9b6da47aba4ae000435
webdemo <kienbkav@gmail.com>
2019-01-31 02:01:15 -0800

    testing

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const app = express();
-const request = require('request');
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html');
-})
-app.all('/post',async (req,res)=>{
 
+app.get('/',async (req,res)=>{
 
-    let option = {
-        method: 'GET',
-        url: 'https://www.sendo.vn/m/wap_v2/search/product?p=1&q=microlab&s=100&search_algo=algo3&sortType=rank',
-        header: req.headers,
-        json:true
-    };
-    req.pipe(request(option)).pipe(res)
+    const got = require('got');
+    let result = '';
+    let response = req.pipe(got.stream('https://whatismyipaddress.com/'));
+    response.on('data',chunked=>result+=chunked.toString('binary'));
+    response.on('end',function(){
+        res.send(result);
+    })
 });
 
-
-
-app.listen(process.env.PORT || 3000)
+app.listen(80)
