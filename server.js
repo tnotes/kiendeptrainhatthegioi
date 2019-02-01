ce1267d65efa15c0f8248b3b950c45e5cf4d3779
webdemo <kienbkav@gmail.com>
2019-01-31 22:53:38 -0800

    testing

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const fs = require('fs');
-const express = require('express');
-const app = express();
-app.use(express.static('public'))
+const request = require("request");
+const app = require('express')();
+app.get('/',async function(req,res){
+    req.headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    };
+    let requestOptions  = {
+        method: "GET",
+        uri: "https://www.whatismyip.com/",
+    };
+    let response = req.pipe(request(requestOptions));
+    let result = '';
+    response.on('data',chunked=>{
+        result += chunked.toString();
+    });
+    response.on('end',function(){
+        res.send(result);
+    })
+    response.on("error", error => {
+        console.error(error.message);
+    });
+});
+
 app.listen(process.env.PORT || 80);
