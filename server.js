0ea2b96b619d152776bd43eabc1a6e04800e4a5f
webdemo <kienbkav@gmail.com>
2019-01-31 23:03:22 -0800

    testing

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const request = require("request");
 const app = require('express')();
 app.get('/',async function(req,res){
-    req.headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-    };
+    req.headers['accept-encoding'] = '';
+    req.headers['accept-language'] = '';
+
     let requestOptions  = {
         method: "GET",
         uri: "https://www.whatismyip.com/",
